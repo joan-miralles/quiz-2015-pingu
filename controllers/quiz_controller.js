7bd582fa4b528e757e6181b3d7cf56c269020886
Joan Miralles <joan.miralles@gmail.com>
2015-07-11 12:59:08 +0200

    Validación de formulario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,13 +21,16 @@ exports.index = function (req, res) {
         search = '%' + search.trim().replace(' ', '%') + '%';
     }
     models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function (quizes) {
-        res.render('quizes/index.ejs', {quizes: quizes});
+        res.render('quizes/index.ejs', {
+            quizes: quizes,
+            errors: []
+        });
     });
 };
 
 //GET /quizes/:quizId
 exports.show = function (req, res) {
-    res.render('quizes/show', { quiz: req.quiz });
+    res.render('quizes/show', { quiz: req.quiz, errors: [] });
 };
 
 //GET /quizes/:quizId/answer
@@ -36,7 +39,11 @@ exports.answer = function (req, res) {
     if (req.query.respuesta === req.quiz.respuesta) {
         resultado = "Correcto";
     }
-    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+    res.render('quizes/answer', {
+        quiz: req.quiz,
+        respuesta: resultado,
+        errors: []
+    });
 };
 
 // GET /quizes/new
@@ -44,15 +51,28 @@ exports.new = function(req, res) {
     var quiz = models.Quiz.build( // crea objeto quiz
         { pregunta: "Pregunta", respuesta: "Respuesta" }
     );
-    res.render('quizes/new', {quiz: quiz});
+    res.render('quizes/new', {
+        quiz: quiz,
+        errors: []
+    });
 };
 
 // PORT /quizes/create
 exports.create = function (req, res) {
     var quiz = models.Quiz.build (req.body.quiz);
 
-    // guarda en DB los campos pregunta y respuesta de quiz
-    quiz.save({fields: ["pregunta", "respuesta"]}).then(function () {
-       res.redirect('/quizes');
-    });   // Redirección HTTP (URL relativo) lista de preguntas
+    quiz.validate().then(
+        function(err) {
+            if (err) {
+                res.render('quizes/new', { quiz: quiz, errors: err.errors });
+            } else {
+                // guarda en DB los campos pregunta y respuesta de quiz
+                quiz.save({fields: ["pregunta", "respuesta"]})
+                    .then(
+                    function () {
+                        res.redirect('/quizes');
+                    });   // Redirección HTTP (URL relativo) lista de preguntas
+            }
+        }
+    );
 };
