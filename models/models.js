9c525446a8ee54495ef76f7f409e8d7142199ecc
Joan Miralles <joan.miralles@gmail.com>
2015-07-11 16:03:19 +0200

    Poder añadir comentarios

--- models/models.js
+++ models/models.js
@@ -29,8 +29,14 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar la definicion de la table Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().success(function () {
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
+});
+
+// Author
+router.get('/author', function (req, res) {
+    res.render('author', { errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -20,9 +26,7 @@ router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
 router.put('/quizes/:quizId(\\d+)',         quizController.update);
 router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
 
-// Author
-router.get('/author', function (req, res) {
-    res.render('author', { errors: [] });
-});
+router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
 
 module.exports = router;
